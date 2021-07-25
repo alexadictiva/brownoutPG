b1d3202cd7213cd69d45890f995186acae2613a6
Libia Perez <alexandra220790@gmail.com>
2021-07-25 20:12:45 -0300

    adding header structure, styles and colors, wrapper sizes,

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
+/*header */
+function darken_screen(yesno) {
+  if (yesno == true) {
+    document.querySelector(".screen-darken").classList.add("active");
+  } else if (yesno == false) {
+    document.querySelector(".screen-darken").classList.remove("active");
+  }
+}
+
+function close_offcanvas() {
+  darken_screen(false);
+  document.querySelector(".mobile-offcanvas.show").classList.remove("show");
+  document.body.classList.remove("offcanvas-active");
+}
+
+function show_offcanvas(offcanvas_id) {
+  darken_screen(true);
+  document.getElementById(offcanvas_id).classList.add("show");
+  document.body.classList.add("offcanvas-active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-trigger]").forEach(function (everyelement) {
+    let offcanvas_id = everyelement.getAttribute("data-trigger");
+
+    everyelement.addEventListener("click", function (e) {
+      e.preventDefault();
+      show_offcanvas(offcanvas_id);
+    });
+  });
+
+  document.querySelectorAll(".btn-close").forEach(function (everybutton) {
+    everybutton.addEventListener("click", function (e) {
+      e.preventDefault();
+      close_offcanvas();
+    });
+  });
+
+  document
+    .querySelector(".screen-darken")
+    .addEventListener("click", function (event) {
+      close_offcanvas();
+    });
+});
+
+/*slide 1*/
 const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
   direction: "vertical",
   loop: true,
-  //autoplay
+
   autoplay: {
-    delay: 2500,
+    delay: 3000,
     disableOnInteraction: false,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
   },
 });
