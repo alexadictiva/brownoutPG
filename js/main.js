1d97302552ea4c7c41b26c0951e7e01727e45188
Libia Perez <alexandra220790@gmail.com>
2021-09-12 13:04:41 -0300

    Fixing the Slider bug

--- js/main.js
+++ js/main.js
@@ -48,12 +48,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /*slide 1*/
-const swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
